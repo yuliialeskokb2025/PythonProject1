cd6a8fbae66867396573089609537f4c690acdd2
Lesko Yulia <leskoyulia@Leskos-MacBook-Air.local>
2026-09-12 17:23:01 -0700

    update print text in main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ def main_func():
     """" The main function of my-project """
     """ Calls function fetch_ip() for fetching IP-address and output result in terminal """
     ip_address = fetch_ip()
-    print("Your IP address is:", ip_address)
+    print("Your IP address is:", ip_address,"!")
 main_func()
