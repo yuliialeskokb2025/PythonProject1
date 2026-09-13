173f0d1791bc8a682977681c335f511731f8824d
Lesko Yulia <leskoyulia@Leskos-MacBook-Air.local>
2026-09-12 17:12:17 -0700

    add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from lib import fetch_ip
+def main_func():
+    """" The main function of my-project """
+    """ Calls function fetch_ip() for fetching IP-address and output result in terminal """
+    ip_address = fetch_ip()
+    print("Your IP address is:", ip_address)
+main_func()
